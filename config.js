3f9567f4525f0019b664c98b1fcf6d09e2bfe3d2
kudodeptrai <85350957+kudodeptrai@users.noreply.github.com>
2021-06-04 20:09:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'minh dit nhe ?.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/kudodeptraihihi/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.fb.com/kudodeptraihihi' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
